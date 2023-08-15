8565bb9023315cb8e0adb6a18137f7079df9c1dc
Vokies <142334060+Vokies@users.noreply.github.com>
2023-08-15 12:19:28 -0400

    Update LOADINGSTRING.lua

--- LOADINGSTRING.lua
+++ LOADINGSTRING.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/Vokies/Charry-blossom-sb-Roblox/main/Slap_Battles.lua")))()
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/Vokies/Charry-blossom-sb-Roblox/main/file")))()
 https://github.com/Vokies/Charry-blossom-sb-Roblox-script/edit/main/LOADINGSTRING.lua
